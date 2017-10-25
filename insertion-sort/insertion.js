21ba2b97f6e97ea28c30ceda6cc28d51d15afec1
Carlo Santos <carlox7@gmail.com>
2017-10-25 13:01:29 -0700

    fixed for loop

--- insertion-sort/insertion.js
+++ insertion-sort/insertion.js
@@ -1,24 +1,13 @@
 'use strict';
 
 module.exports = function insertionSort(arr){
-  for(let i = 1; i < arr.length; i++){
+  let j, i;
+  for(i = 1; i < arr.length; i++){
     let temp = arr[i];
-    for(var j = i - 1; j >= 0 && (arr[j] > temp); j--){
+    for(j = i - 1; j >= 0 && (arr[j] > temp); j--){
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = temp;
   }
   return arr;
 };
-
-// module.exports = function insertionSort(unsortedList) {
-//   var len = unsortedList.length;
-//   for (var i = 1; i < len; i++) {
-//     var tmp = unsortedList[i];
-//     for (var j = i - 1; j >= 0 && (unsortedList[j] > tmp); j--) {
-//       unsortedList[j + 1] = unsortedList[j];
-//     }
-//     console.log(unsortedList[j + 1]);
-//     unsortedList[j + 1] = tmp;
-//   }
-// };
